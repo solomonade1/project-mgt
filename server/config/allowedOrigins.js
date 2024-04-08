9789b00cfd8b84e45e2ff67dce7b34421437cdfb
solomonade1 <solomonadegboyega1@gmail.com>
2024-04-09 00:23:18 +0100

    fix/ origin cors error fixed

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,1 +1,1 @@
-export const allowedOrigins = ["https://project-mgt-o9ut.onrender.com/", "http://localhost:3000/"];
+export const allowedOrigins = ["https://project-mgt-o9ut.onrender.com", "http://localhost:3000"];
